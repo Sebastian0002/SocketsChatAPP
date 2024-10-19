a89f04b29ec023f036e81b6401f41a6a4bfcd4af
jhoann martinez <jhoann022@gmail.com>
2024-10-18 21:56:41 -0500

    mvp

--- ChatApp/lib/constants/envarioments.dart
+++ ChatApp/lib/constants/envarioments.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 final bool _isAndroid = Platform.isAndroid;
-final String _ipAddress = _isAndroid ? "192.168.1.72" : "localhost";
+final String _ipAddress = _isAndroid ? "YOUR_IP_ADDRESS" : "localhost";
 
 class Enviroment {
   static String apiBaseUrl = 'http://$_ipAddress:3000/api';
